8cbbdba3b71d70e81198bcaf80b5f7eccd12d01e
ohm0106 <134082781+ohm0106@users.noreply.github.com>
2024-06-02 19:27:43 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T; 
+	cin >> T;
+
+	vector<pair<int, int>> A(T);
+
+	for (int i = 0; i < T; i++) {
+		cin >> A[i].first >> A[i].second;
+	}
+
+	for (int i = 0; i < T; i++) {
+		cout << "Case #"<< i+1 << ": " << A[i].first + A[i].second << "\n";
+	}
+}
