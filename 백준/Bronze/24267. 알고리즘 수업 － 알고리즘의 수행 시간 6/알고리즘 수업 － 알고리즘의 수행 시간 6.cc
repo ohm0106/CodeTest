acab9c305aad96cb4043a9aea3c074f303e8760e
ohm0106 <134082781+ohm0106@users.noreply.github.com>
2024-06-14 13:26:21 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	long long int num;
+	cin >> num;
+	long long int numOfInteration = num * (num - 1) * (num - 2) / 6;
+	cout << numOfInteration << "\n";
+	cout << 3;
+}
