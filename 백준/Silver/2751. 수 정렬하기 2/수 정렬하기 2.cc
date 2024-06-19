e3badd5a8b6996255ca5287d115121395a4ccc03
ohm0106 <134082781+ohm0106@users.noreply.github.com>
2024-06-19 15:16:17 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 272 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int N;
+	cin >> N;
+
+	vector<int> A(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	sort(A.begin(), A.end());
+
+	for (const auto& p : A) {
+		cout << p << "\n";
+	}
+		
+}
